b27407ba57a09867c74dfdfb1d7828b575b34f7a
wangwen1220 <wangwen1220@139.com>
2014-11-20 17:41:17 +0800

    work

--- config.php
+++ config.php
@@ -3,13 +3,13 @@
 define('ABSPATH', dirname(__FILE__) . '/');
 // $TEMPLATE = 'template/default/';
 
+$name = 'en.OFweek.com';
+$title = '首页';
+$keywords = '关键字';
+$description = '描述';
+// $islogin = 'false';
+// $uid = '123456';
+
 // 重定向
 // header('Location: template/default/');
 // exit;
-
-$name = 'OFweek';
-$title = '首页';
-$keywords = 'OFweek 关键字';
-$description = 'OFweek 描述';
-$islogin = 'false';
-$uid = '123456';
