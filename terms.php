49e33499b01ab6e2cb0d4b661a5f63f1881e7c3b
wangwen1220 <wangwen1220@139.com>
2015-01-29 09:08:16 +0800

    'office'

---
+++ terms.php
@@ -0,0 +1,82 @@
+<?php
+require_once 'config.php';
+
+$class = 'terms nocart nosearch';
+$title = 'Terms';
+$keywords = 'Terms';
+$description = 'Terms';
+
+include 'header.html';
+?>
+
+  <div id="main">
+    <div class="pagetitle">Reset Password</div>
+    <div class="terms">
+      <p>Welcome to OFweek.com. The following document explains the terms and conditions applicable to your use of OFweek.com. Because the Terms and Conditions contain legal obligations, please read them carefully.</p>
+
+      <h2>1. Acceptance</h2>
+      <p>By accessing OFweek.com or using services provided by OFweek.com, you acknowledge the terms and conditions and agree that you will be bound by the terms and conditions contained in this Agreement. In addition, this Agreement may be changed or updated by OFweek.com without any prior notice to you. Changed terms and conditions will become valid upon being posted on OFweek.com. Your continuous use of our service signifies your acceptance of the change terms. Unless explicitly stated otherwise, any new functions that be added to OFweek.com shall be subject to this Agreement.</p>
+
+      <h2>2. Conditions and Restrictions on Use</h2>
+      <p>OFweek.com provides services to individuals and companies who can form legally binding contracts under applicable law. If you do not qualify, please do not use the service. Any user on OFweek.com must behave in accordance with applicable laws. Your use of the Products and Services is subject to compliance with these Terms and Conditions. You acknowledge and agree that OFweek.com may terminate your access to OFweek.com or to any of the Products and Services should you fail to comply with the Terms and Conditions. Any such termination shall be in OFweek.com's sole discretion and may occur without prior notice, or any notice. OFweek.com further reserves the right to terminate any user's access to OFweek.com or to any of the Products and Services for any conduct that OFweek.com, in its sole discretion, believes is or may be directly or indirectly harmful to other users, to OFweek.com or its subsidiaries, affiliates, or to other third parties, or for any conduct that violates any local, or foreign laws or regulations.</p>
+
+      <h2>3. Registration Obligations</h2>
+      <p>In joining or becoming a member of OFweek.com, you hereby agree to:</p>
+      <p>(a) provide true, accurate, current and complete information about yourself in the "JOIN NOW" application form (such information being the "Registration Content") and (b) maintain and promptly update the Registration Content to keep it true, accurate, current and complete. If you provide any information that is untrue, inaccurate, not current or incomplete, or OFweek.com has reasonable grounds to suspect that such information is untrue, inaccurate, not current or incomplete, OFweek.com reserves the right to suspend or terminate your member service.</p>
+
+      <h2>4. Member ID, Password and Security</h2>
+      <p>During the registration process, you will create a Member ID and Password. You are solely responsible for maintaining the confidentiality of your Member ID and Password, and are fully responsible for all activities that occur under your Member ID and Password. You agree to (a) immediately notify OFweek.com of any unauthorized use of your Member ID or Password or any other breach of security, and (b) ensure that you properly exit the website at the end of each session. OFweek.com cannot and will not be liable for any loss or damage arising from your failure to comply with this Agreement.</p>
+
+      <h2>5. Information collected by OFweek.com</h2>
+      <p>Only registered members can post information on OFweek.com. OFweek.com reserves the right to select and delete information it deems inappropriate. Every member is solely & legally responsible for the accuracy, completeness, integrity, timeliness, correctness and non-infringement of the information provided. Though OFweek.com makes great effort to confirm the information provided, OFweek.com does not guarantee & is not legally responsible for the accuracy, integrity, quality & non-infringement of such information. OFweek.com collects information about companies, products & offers, and builds its own database. OFweek.com holds copyright over all related information and web designs. Any unauthorized copying or commercial use of content obtained from OFweek.com is strictly prohibited, and OFweek.com reserves right to take legal action in the case of such activity.</p>
+
+      <h2>6. Third Parties and Content</h2>
+      <p>You acknowledge and agree that some of the content like data, files, texts, graphics, photos, videos, logos, banners, advertising materials and other content materials, displayed on OFweek.com is provided or posted by third parties or other Users. Any content provided or posted by third parties or other Users, whether publicly posted or privately transmitted, is the sole responsibility of the party or the person from which such content originated. In other words, User, and not OFweek.com, is entirely responsible for all content that he/she uploads, posts, emails or otherwise transmits via the features and services provided by OFweek.com.</p>
+      <p>OFweek.com does not control the user or third party content posted via the products and services. As such OFweek.com is not responsible for and does not guarantee the accuracy, integrity, quality, propriety, or lawfulness of any third party or User content, and shall not be liable to any User in connection with such User's reliance of such third party content. In addition, OFweek.com is not responsible for the conduct of any User's activities on the Site, and shall not be liable to any person in connection with any damage suffered by any person as a result of such User's conduct. As a general condition, although OFweek.com reserves the right to do so, OFweek.com does not pre-screen user or third party content posted on this Site. OFweek.com does not guarantee that any screening will be done to your satisfaction or that any screening will be done at all. OFweek.com reserves the right to monitor some, all, or no areas of the features, products and services for adherence to these Terms & Conditions.</p>
+      <p>OFweek.com may contain hyperlinks in the form of word link, banners or otherwise, to other websites that are controlled by parties other than OFweek.com. Users must be aware that OFweek.com is not responsible for any use of such other websites, even when said website is accessed through a link from the OFweek.com. OFweek.com does not control these websites, does not monitor such sites and bears no responsibility or liability for, nor should OFweek.com be deemed in any way to endorse or approve of, the contents, products, services and information on the linked website.</p>
+      <p>You must be aware that messages or information sent by other Users through communication systems provided by the Site or through emails and fax or letters to addresses obtained from the Site is out of OFweek.com's control and monitoring ability and so you acknowledge that OFweek.com can not be held responsible or liable for the content of these messages and information. The message sender shall be responsible for the content of all the messages communicated, as well as the consequences of any such message.</p>
+      <p>You acknowledge and agree that by using the features, products and services provided on OFweek.com, you may be exposed to content that is offensive, indecent or objectionable. Under no circumstances will OFweek.com be liable in any way for any user or third party content, including, but not limited to, for any errors or omissions in any such content, or for any loss or damage of any kind incurred as a result of the use of any such content posted, emailed or otherwise transmitted via the features, products and services provided by the Site.</p>
+
+      <h2>7. Charge</h2>
+      <p>Access to OFweek.com and registering Free Member or Buyer Member are free of charge. OFweek.com reserves the right to modify the charging clause and we promise to notify you of the changed clause immediately. Golden Member, Platinum Member or users who choose advertisement services or other charge services on OFweek.com should pay under the contract conditions signed with OFweek.com. OFweek.com reserves the right to modify the charging clause. Please note that we are not responsible for all hardware, software, service and other costs you incur while accessing our website and Internet based data on our servers. OFweek.com reserves the right to change or discontinue, temporarily or permanently, some or all of the services at any time without notice.</p>
+
+      <h2>8. Rules of Conduct</h2>
+      <p>By using OFweek.com, you warrant and agree to provide OFweek.com with true, accurate, current and complete information to be displayed on OFweek.com and maintain and promptly amend all information to keep it true, accurate, current and complete.</p>
+      <p>You grant an irrevocable, perpetual, worldwide and royalty-free, sub-licensable license to OFweek.com, to display, use and reuse all information provided by you in accordance with the purposes set forth in these Terms & Conditions.</p>
+      <p>You warrant and agree that you have obtained all necessary third party copyright or trademark licenses and permissions and shall be solely responsible for ensuring that any material or information or images you post on PCbay.com or provides to OFweek.com or authorizes OFweek.com to display, do not violate the copyright, trademark, trade secret or any other personal or proprietary rights of any third party, or is posted with the permission of the owners of such rights.</p>
+
+      <h2>9. Termination</h2>
+      <p>OFweek.com reserves the right to terminate your service at any time or for any reason, with or without notice, if it concludes that you have provided false information in connection with your member account, are engaged in fraudulent or illegal activities, or are engaged in activities that may damage the rights of OFweek.com or others. OFweek.com withholds the right to temporarily or permanently terminate your membership for any of the following reasons where you:</p>
+      <ol>
+        <li>(A) Send spam messages or repeatedly post the same product information.</li>
+        <li>(B) Post any material that is not related to international trade or business cooperation.</li>
+        <li>(C) Impersonate or unlawfully use another companies name to post information or conduct business of any form.</li>
+        <li>(D) Any unauthorized access, use, modification or control of the OFweek.com database, network or related services.</li>
+        <li>(E) Obtain by any means other member's username and/or password.</li>
+        <li>(F) Posting, uploading, emailing or transmitting any trade leads, information, data, text, files, links, chat, communication, or any other materials or any content that are unlawful, harmful, illegal, threatening, abusive, intruding another's privacy, slanderous, vulgar, racist, embarrassing or otherwise objectionable to any other person or entity as determined by OFweek.com.</li>
+        <li>(G) Collecting or storing personally identifying information about other users for unlawful or unauthorized purposes</li>
+        <li>(H) Being obscene or containing any pornography or sex-related merchandising or any other content or otherwise promoting sexually explicit materials</li>
+      </ol>
+
+      <h2>10. Liability Disclaimer</h2>
+      <p>OFweek.com will not be liable for interruption to service as a result of acts of god, hardware malfunction, software related problems, or human error. The services available to you on OFweek.com may contain errors and our service is subject to periods of interruption. While OFweek.com does its best to maintain the services available on OFweek.com cannot be held responsible for any errors, defects, lost profits, or other consequential damages arising from the use of this website.</p>
+      <p>If you are dissatisfied with OFweek.com, or any portion, your exclusive remedy shall be to cease using the website.</p>
+
+      <h2>11. Disclaimer of Warranties for Products Database</h2>
+      <p>You expressly agree that use of our database contained on OFweek.com is at your own risk. OFweek.com shall not be responsible for any content found on this database. OFweek.com expressly disclaims all warranties of any kind, whether implied, including, but not limited to, the implied warranties of merchantability, fitness for a particular purpose, and non-infringement. OFweek.com makes no warranty that our database will be uninterrupted, timely, securely or error free.</p>
+      <p>You acknowledge and agree that any material, and/or data downloaded or otherwise obtained through use of the database on OFweek.com is done at your own discretion and risk. You will be solely responsible for any damages to your computer system or loss of data that result from the download of such material and/or data.</p>
+      <p>OFweek.com, its officers, directors, owners, agents and employees, shall not be liable to you or anyone else for any loss or injury resulting from using of Products on OFweek.com, or for any decision made or action taken by you in reliance on content contained in Products.</p>
+
+      <h2>12. Security</h2>
+      <p>OFweek.com adopts strict security policies. If any member should interfere with our security policies they shall be temporarily or permanently banned from using OFweek.com and its related services. In addition information published by other members will not be available to those members whose access to OFweek.com has been terminated.</p>
+
+      <h2>13. Indemnification</h2>
+      <p>You agree to indemnify and hold OFweek.com, its officers, directors, owners, agents and employees, harmless from any claim or demand, including reasonable attorneys' fees, made by any third party due to or arising out of your use of the Products on OFweek.com, the violation of these terms and conditions by you, or the infringement by you, or other user of Products using your computer, of any intellectual property or other right of any person or entity. OFweek.com reserves the right, at its own expense, to assume the exclusive defense and control of any matter otherwise subject to indemnification by you.</p>
+
+      <h2>14. Copyright</h2>
+      <p>All contents in these websites are Copyright © by OFweek.com.</p>
+      <p>Reproduction in whole or in part in any form without the written permission of OFweek.com is prohibited. OFweek.com all rights reserved.</p>
+    </div>
+  </div>
+
+<?php include 'footer.html'; ?>
